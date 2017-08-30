3b860fb63cf987b2407dbb899a4802a827533000
Quintin Balsdon <qbalsdon@gmail.com>
2017-08-30 16:25:29 +0000

    Got the MongoDB relationship working

--- Server/server.js
+++ Server/server.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+var url = 'mongodb://localhost:27017/sensornet';
 
 var app = express();
 app.use("/scripts", express.static(__dirname + '/scripts'));
 app.use(bodyParser.urlencoded({extended: true}))
 
+function connect(url) {
+  return new Promise(function(resolve, reject) {
+    MongoClient.connect(url, function(err, db) {
+      if (err) {
+        console.error('mongo connection error: ', err.message);
+        reject(err);
+      } else {
+        console.info("connected to mongodb");
+        resolve(db);
+      }
+    });
+  });
+}
+
 app.get('/', function(req, res) {
     res.sendFile('index.html', {root: __dirname })
 });
 
-app.listen(3000, function() {
-  console.log('Server running on port 3000')
-})
+connect(url).then(function(db) {
+  app.get('/data', function (req, res) {
+    // do something with db
+    res.send('Node.js and MongoDB up and running');
+  });
 
-MongoClient.connect('link-to-mongodb', (err, database) => {
-  // ... start the server
-})
+  app.listen(3000, function () {
+    console.log('Server started on port 3000!');
+  });
+}, function(err) {
+  console.error(err);
+});
+
